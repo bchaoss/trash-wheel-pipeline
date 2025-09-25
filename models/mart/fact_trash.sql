46da3244e3e6d7eb82001494a0eaed22d768daf3
bchaoss <103650247+bchaoss@users.noreply.github.com>
2025-09-26 07:53:13 +0800

    Update fact_trash.sql

--- models/mart/fact_trash.sql
+++ models/mart/fact_trash.sql
@@ -1,3 +1,15 @@
-select
-* 
-from {{ ref('stg_trash_clean') }}
+SELECT
+  dumpster_id
+  , date
+  , weight
+  , volume
+  , plastic_bottles
+  , polystyrene
+  , cigarette_butts
+  , glass_bottles
+  , plastic_bags
+  , wrappers
+  , sports_balls
+  , homes_powered
+FROM 
+  {{ ref('stg_trash_clean') }}
