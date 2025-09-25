08b2790ed705fb40e1bb8b637dca98ead395b096
bchaoss <103650247+bchaoss@users.noreply.github.com>
2025-09-26 07:22:19 +0800

    Update stg_trash_clean.sql

--- models/staging/stg_trash_clean.sql
+++ models/staging/stg_trash_clean.sql
@@ -1,3 +1,9 @@
-select
-*
-from {{ ref('ingest_trash_wheel') }}
+SELECT 
+  CONCAT(SUBSTRING(b.sheet_name, 1, 1), dumpster) AS dumpster_id
+  , CASE WHEN CAST(date AS VARCHAR) LIKE '00%'
+         THEN CAST(CONCAT('20', SUBSTRING(CAST(date AS VARCHAR), 3)) AS DATE)
+    ELSE date END AS date
+  , a.* EXCLUDE (dumpster, _month, _year, date, filename) 
+from {{ ref('ingest_trash_wheel') }} a
+  left join main.files_to_read b on a.filename=b.csv_link
+
