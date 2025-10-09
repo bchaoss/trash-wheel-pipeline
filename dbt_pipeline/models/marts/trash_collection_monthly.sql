6aba0b35ad54f0cff6693f17cd6ec04d761d4863
bchaoss <103650247+bchaoss@users.noreply.github.com>
2025-10-10 05:41:21 +0800

    Update trash_collection_monthly.sql

--- dbt_pipeline/models/marts/trash_collection_monthly.sql
+++ dbt_pipeline/models/marts/trash_collection_monthly.sql
@@ -20,19 +20,21 @@ from (
     wheel_id
     , year
     , month
+    , date
     , case when month in (12, 1, 2) then 'Winter'
            when month in (3, 4, 5) then 'Spring'
            when month in (6, 7, 8) then 'Summer'
            when month in (9, 10, 11) then 'Fall'
       end as season
-    , count(dumpster_id) as num_dumpster
+    , cast(load_time as date) as load_time
     
+    , count(dumpster_id) as num_dumpster
     {% for col in cols_to_sum %}
     , sum({{ col }}) as {{ col }}
     {% endfor %}
     
   from {{ ref('fact_trash_collection') }}
-  group by 1, 2, 3, 4
+  group by 1, 2, 3, 4, 5, 6
 ) a
 left join {{ ref('dim_trash_wheel') }} b 
   on a.wheel_id = b.wheel_id
