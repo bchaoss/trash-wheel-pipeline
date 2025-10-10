ff16833e3a24a55b6f84c4c5f5475df169c4df85
bchaoss <103650247+bchaoss@users.noreply.github.com>
2025-10-10 11:19:40 +0800

    Update page detail

--- evidence_bi/pages/+layout.svelte
+++ evidence_bi/pages/+layout.svelte
@@ -1,3 +1,10 @@
-<EvidenceDefaultLayout {data} hideSidebar=true >
+<script>
+	import '@evidence-dev/tailwind/fonts.css';
+	import '../app.css';
+	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	export let data;
+</script>
+
+<EvidenceDefaultLayout {data} hideSidebar=true hideBreadcrumbs=true githubRepo='https://github.com/bchaoss/trash-wheel-analysis' >
 	<slot slot="content" />
 </EvidenceDefaultLayout>
